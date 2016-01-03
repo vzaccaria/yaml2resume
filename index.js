d469c9514efb75dcc8be04f22016ecf430a014a1
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2016-01-03 13:31:21 +0100

    Add support for includes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
-/* eslint quotes: [0], strict: [0] */
 "use strict";
+
+var _createClass = (function () { function defineProperties(target, props) { for (var key in props) { var prop = props[key]; prop.configurable = true; if (prop.value) prop.writable = true; } Object.defineProperties(target, props); } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _inherits = function (subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; };
+
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
+/* eslint quotes: [0], strict: [0] */
 
 var _require = require("zaccaria-cli");
 
@@ -14,6 +21,10 @@ var $s = _require.$s;
 var _ = _require._;
 var $fs = _require.$fs;
 
+var debug = require("debug")("yaml2resume");
+var Promise = $b;
+var path = require("path");
+
 var yaml = require("js-yaml").safeLoad;
 var liquid = require("liquid-node");
 
@@ -24,12 +35,36 @@ var getOptions = function (doc) {
     var template = $o("-t", "--template", "latex-jr", o);
     var latex = $o("-l", "--latex", false, o);
     var json = $o("-j", "--json", false, o);
-    var isAbsolute = template.charAt(0) == "." || template.charAt(0) == "/";
+    var c = template.charAt(0);
+    var isAbsolute = c !== "." || c !== "/";
     var list = _.get(o, "list", false);
     return {
         help: help, template: template, latex: latex, list: list, json: json, isAbsolute: isAbsolute
     };
 };
+
+var CustomFileSystem = (function (_liquid$BlankFileSystem) {
+    function CustomFileSystem() {
+        _classCallCheck(this, CustomFileSystem);
+
+        if (_liquid$BlankFileSystem != null) {
+            _liquid$BlankFileSystem.apply(this, arguments);
+        }
+    }
+
+    _inherits(CustomFileSystem, _liquid$BlankFileSystem);
+
+    _createClass(CustomFileSystem, {
+        readTemplateFile: {
+            value: function readTemplateFile(pth) {
+                var fileName = path.resolve("" + process.cwd() + "/includes/" + pth + ".liquid");
+                return $fs.readFileAsync(fileName, "utf-8");
+            }
+        }
+    });
+
+    return CustomFileSystem;
+})(liquid.BlankFileSystem);
 
 var main = function () {
     $f.readLocal("docs/usage.md").then(function (it) {
@@ -45,6 +80,7 @@ var main = function () {
         if (help) {
             console.log(it);
         } else {
+            debug("Parsing options");
             if (list) {
                 console.log("Available templates:");
                 console.log($s.ls("" + __dirname + "/templates"));
@@ -52,12 +88,16 @@ var main = function () {
             }
             var filePromise = undefined;
             if (isAbsolute) {
+                debug("Absolute path template detected");
                 filePromise = $fs.readFileAsync(template, "utf-8");
             } else {
+                debug("Normal path template filename ");
                 filePromise = $f.readLocal("templates/" + template);
             }
             $b.all([$r.stdin(), filePromise]).spread(function (data, file) {
+
                 var engine = new liquid.Engine();
+                engine.fileSystem = new CustomFileSystem();
                 if (json) {
                     data = JSON.parse(data);
                 } else {
@@ -78,13 +118,21 @@ var main = function () {
                             $s.cp(__dirname + "/latex-classes/*", path);
                             var compile = function () {
                                 console.log("xelatex " + file);
-                                return $s.execAsync("xelatex " + file, { silent: true });
+                                return $s.execAsync("xelatex " + file, {
+                                    silent: true
+                                });
                             };
                             return compile().then(compile).then(function () {
                                 $s.cp("-f", "file.pdf", "" + cwd + "/resume.pdf");
                             });
-                        }, { unsafeCleanup: true });
+                        }, {
+                            unsafeCleanup: true
+                        });
+                    } else {
+                        console.log(it);
                     }
+                })["catch"](function (err) {
+                    console.log("Error: " + err);
                 });
             });
         }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ var {
     $d, $o, $f, $r, $b, withTmpDir, $s, _, $fs
 } = require('zaccaria-cli')
 
+let debug = require('debug')('yaml2resume')
+let Promise = $b
+let path = require('path')
+
+
 let yaml = require('js-yaml').safeLoad;
 let liquid = require('liquid-node');
 
@@ -13,12 +18,23 @@ var getOptions = doc => {
     var template = $o('-t', '--template', 'latex-jr', o);
     var latex = $o('-l', '--latex', false, o);
     var json = $o('-j', '--json', false, o);
-    var isAbsolute = template.charAt(0) == '.' || template.charAt(0) == '/';
+    var c = template.charAt(0)
+    var isAbsolute = (c !== '.') || ( c !== '/')
     var list = _.get(o, 'list', false)
     return {
         help, template, latex, list, json, isAbsolute
     }
 }
+
+class CustomFileSystem extends liquid.BlankFileSystem {
+
+    readTemplateFile(pth) {
+        let fileName = path.resolve(`${process.cwd()}/includes/${pth}.liquid`);
+        return $fs.readFileAsync(fileName, 'utf-8');
+    }
+}
+
+
 
 var main = () => {
     $f.readLocal('docs/usage.md').then(it => {
@@ -28,20 +44,25 @@ var main = () => {
         if (help) {
             console.log(it)
         } else {
-            if(list) {
+            debug("Parsing options");
+            if (list) {
                 console.log("Available templates:");
                 console.log($s.ls(`${__dirname}/templates`))
                 process.exit(0);
             }
             let filePromise;
-            if(isAbsolute) {
+            if (isAbsolute) {
+                debug("Absolute path template detected");
                 filePromise = $fs.readFileAsync(template, 'utf-8')
             } else {
+                debug("Normal path template filename ");
                 filePromise = $f.readLocal(`templates/${template}`)
             }
             $b.all([$r.stdin(), filePromise]).spread((data, file) => {
+
                 let engine = new liquid.Engine
-                if(json) {
+                engine.fileSystem = new CustomFileSystem
+                if (json) {
                     data = JSON.parse(data)
                 } else {
                     data = yaml(data);
@@ -58,16 +79,24 @@ var main = () => {
 
                             it.to(file);
                             $s.cd(path);
-                            $s.cp(__dirname+'/latex-classes/*', path)
+                            $s.cp(__dirname + '/latex-classes/*', path)
                             let compile = () => {
                                 console.log(`xelatex ${file}`);
-                                return $s.execAsync(`xelatex ${file}`, { silent: true })
+                                return $s.execAsync(`xelatex ${file}`, {
+                                    silent: true
+                                })
                             }
                             return compile().then(compile).then(() => {
                                 $s.cp('-f', 'file.pdf', `${cwd}/resume.pdf`);
                             })
-                        }, { unsafeCleanup: true })
+                        }, {
+                            unsafeCleanup: true
+                        })
+                    } else {
+                        console.log(it);
                     }
+                }).catch((err) => {
+                    console.log(`Error: ${err}`);
                 })
             })
         }
